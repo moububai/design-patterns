3921c09fed27bf36e37b952d5ad96bc949e33ad6
mobbi <nevocamo@gmail.com>
2023-09-30 02:55:43 -0400

    added javadoc

--- singleton/Anagram.java
+++ singleton/Anagram.java
@@ -2,23 +2,57 @@ package singleton;
 
 import java.util.ArrayList;
 
+/*
+ * A new anagram is created, that is comparable 
+ * 
+ * An anagram is a word with the letters rearranged
+ * question - rearranged word
+ * answer - arranged word
+ * 
+ * @author Cameron Reyes
+ * @version 1.0 build Sept 30, 2023
+ */
 public class Anagram {
 
     private String question;
     private ArrayList<String> answers;
+
+    /*
+     * Creates an anagram given the rearrangment, and the corresponding arrangmenets
+     * 
+     * An anagram is a word with the letters rearranged
+     * 
+     * @param question       The rearranged wording of the Anagram
+     * @param answer         The arranged wording(s) of the Anagram. Multiple
+     */
 
     public Anagram(String question, ArrayList<String> answers){
         this.question = question;
         this.answers = answers;
     }
 
+    /*
+     * Allows user to see the rearrangement of the Anagram
+     * 
+     * @return question     The rearranged wording of the Anagram
+     */
+
     public String getQuestion(){
         return question;
     }
 
+    /*
+     * Allow's user to see if their answer is a valid arrangment of the Anagram 
+     * 
+     * The valid arragment of the Anagram is contained in the Arraylist answers, this case is not
+     * case sensitive.
+     * 
+     * @return boolean      returns True if the user's answer is a match with the arranged Anagrams
+     */
+
     public boolean isCorrect(String userAnswer){
         for(String answer: answers){
-            if (answer.equals(userAnswer)){
+            if (answer.toLowerCase().equals(userAnswer.toLowerCase())){
                 return true;
             }
         }
--- singleton/Game.java
+++ singleton/Game.java
@@ -4,6 +4,16 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Random;
 
+/*
+ * Allows for user to play a game of Anagrams, finding arrangments of rearranged words. 
+ * 
+ * An anagram is a rearragment of an arranged words. There is a singleton meaning, only one
+ * instance of the game is ran. 
+ * 
+ * @author Cameron Reyes
+ * @version 1.0 build Sept 30, 2023
+ */
+
 public class Game {
 
     private static Game game;
@@ -13,6 +23,13 @@ public class Game {
     private Anagram currentAnagram;
     private int score;
 
+
+    /*
+     * Ensures that only one Game class is constructed
+     * 
+     * Loads the HashMap with each difficulty, random is initiazlied 
+     */
+
     private Game(){
         anagrams = new HashMap<>();
         anagrams.put(Difficulty.EASY, FileReader.getAnagrams(Difficulty.EASY));
@@ -21,6 +38,13 @@ public class Game {
         rand = new Random();
     }
 
+    /*
+     * Loads for the class to be working so the class does not destory when complete 
+     * 
+     * 
+     * @param game      The instantiation of the class is loaded
+     */
+
     public static Game getInstance(){
         if(game == null){
             game = new Game();
@@ -28,11 +52,26 @@ public class Game {
         return game;
     }
 
+    /*
+     * Loads a random anagram, the rearragnment is given to the user
+     * 
+     * The level is checked when this method is called, to determine the difficulty of the Anagram(3,4,or 5 letters).
+     * 
+     * @param currentAnagram.getQuestion()      Returns in string the current rearrangment of an Anagram
+     */
+
     public String getQuestion(){
         checkLevel();
         currentAnagram = (anagrams.get(level)).get(rand.nextInt(anagrams.get(level).size()));
         return currentAnagram.getQuestion();
     }
+
+    /*
+     * Loads the current Difficulty the user is on
+     * 
+     *  This method is used when creating a question 
+     * 
+     */
 
     private void checkLevel(){
         if (score >= 10){
@@ -46,6 +85,15 @@ public class Game {
             }
     }
 
+    /*
+     * Allows user to see if their answer for a rearranged Anagram, matches an arragemnet of that Anagram
+     * 
+     * This allows the score to be updated: when the user's answer is right the score decreases, when the score
+     * get's higher the score increases.
+     * 
+     * @pram boolean        returns true if the user's answer matches a correct Arragnemnt of the Anagram
+     */
+
     public boolean isCorrect(String userAnswer){
         if (currentAnagram.isCorrect(userAnswer)){
             score++;
@@ -54,6 +102,14 @@ public class Game {
         score--;
         return false;
     }
+    
+    /*
+     * Allows the user to see the current score the user is on
+     * 
+     * The score determines the current Difficulty the Anagram is displaying(3, 4, or 5 letters).
+     * 
+     * @return score        The current right answer's the user has minus their incorrect ones
+     */
 
     public int getScore(){
         return score;
